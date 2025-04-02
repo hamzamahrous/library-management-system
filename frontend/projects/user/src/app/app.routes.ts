4c1c58020d72bff472f0604218a4b522f01c8ddd
hamzamahrous <hamza.mahrous11@gmail.com>
2025-04-02 14:54:03 +0200

    Removed all home related components

--- frontend/projects/user/src/app/app.routes.ts
+++ frontend/projects/user/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
-import { HomeComponent } from './home/home.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 export const routes: Routes = [
