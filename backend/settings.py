b07758f60d2f77152363ae3a235fddac221efe09
ahmeda335 <ahmedadel30320032003@gmail.com>
2025-05-09 00:15:23 +0300

    Adding gitignore for python

--- backend/settings.py
+++ backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.environ.get("DJANGO-SECRET-KEY")
 SECRET_KEY = "django-insecure-n$bw(-q#xh#8q((319*bao8*%ia-p*366wp57m%4m00^zu+huy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
