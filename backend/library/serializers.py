5d5f06efd61205c7dde729eaf5815861cda8b4b0
ahmeda335 <ahmedadel30320032003@gmail.com>
2025-05-17 19:17:20 +0300

    Adding user detail

--- backend/library/serializers.py
+++ backend/library/serializers.py
@@ -20,13 +20,15 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True,}}
 
     def create(self, validated_data):
         user = User(
             username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
         user.set_password(validated_data['password'])
         user.save()
